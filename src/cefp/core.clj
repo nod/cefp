b3410774ac2c357112a44d3ade4f19da4750f1b6
Jarrod Baumann <jarrod@theos.org>
2013-07-17 22:58:27 -0500

    style change

--- src/cefp/core.clj
+++ src/cefp/core.clj
@@ -1,23 +1,18 @@
 (ns cefp.core
-  (:use [clojure.string :only [trim split]])
-    )
+  (:use [clojure.string :only [trim split]]))
 
 (defn cef-find-kv [cefs]
-  (last (split cefs #"\|" 8))
-  )
+  (last (split cefs #"\|" 8)))
 
 (defn cef-keys [s]
-  (map #(keyword (trim (second %))) (re-seq #"(\w+)=" s))
-  )
+  (map #(keyword (trim (second %))) (re-seq #"(\w+)=" s)))
 
 (defn cef-values [s]
-  (map #(trim %) (drop 1 (split s #"(\w+)=")))
-  )
+  (map #(trim %) (drop 1 (split s #"(\w+)="))))
 
 (defn cef-parse [cefs]
   (let [s (cef-find-kv cefs)]
-    (zipmap (cef-keys s) (cef-values s)))
-  )
+    (zipmap (cef-keys s) (cef-values s))))
 
 (def cefsample "|||||||k=v")
 (def biggersample "|||||||k=v somedog=my pet cat")
