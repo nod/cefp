6d2576c33b79784c0db847ae13dc48da09a665ab
Jarrod Baumann <jarrod@theos.org>
2013-07-17 22:44:09 -0500

    different approach

--- test/cefp/test/core.clj
+++ test/cefp/test/core.clj
@@ -1,5 +1,5 @@
 (ns cefp.test.core
-  (:use [cefp.core :only (cef-parse cef-find-kv keyval symval)])
+  (:use [cefp.core :only (cef-parse cef-find-kv)])
   (:use [clojure.test]))
 
 (deftest test-cef-find-kv
@@ -8,17 +8,7 @@
     (is (= "k2=v\\|k3=4" (cef-find-kv "|||||||k2=v\\|k3=4")))
 	)
 
-(deftest test-keyval
-	(is (= [["k" "v"]] (keyval "k=v")))
-	(is (= [["k" "v"] ["x" "y"]] (keyval "k=v x=y")))
-	(is (= [["k" "v"] ["x" "y"] ["somedog" "mycat"]]
-		(keyval "k=v x=y somedog=mycat")))
-	)
-
-(deftest test-symval
-	(is (= [:k "v"] (symval "k" "v")))
-	)
-
 (deftest test-cef-parse
 	(is (= {:k "v"} (cef-parse "|||||||k=v")))
+        (is (= {:what "what what"} (cef-parse "|||||||what=what what")))
 	)
